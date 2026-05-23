d1b2563ae4c6351e7eb351ca10510d47fceb30bd
Pedro <pedrojr.ferreiraa@gmail.com>
2026-05-23 20:49:10 -0300

    Update PlanoAtividadeEditor.tsx

--- src/components/atividade/PlanoAtividadeEditor.tsx
+++ src/components/atividade/PlanoAtividadeEditor.tsx
@@ -486,8 +486,8 @@ export function PlanoAtividadeEditor({ modo }: { modo: "regular" | "pcd" }) {
             nome: alunoFoco.primeiro_nome,
             codigo: alunoFoco.pcd_codigo || undefined,
             anotacoes: alunoFoco.pcd_anotacoes || undefined,
-            peiResumo: peiResumoByStudent?.[alunoFoco.id] || undefined,
-            anamneseResumo: anamneseResumoByStudent?.[alunoFoco.id] || undefined,
+            peiResumo: peiResumoBySlug[alunoFoco.id] || undefined,
+            anamneseResumo: anamResumoBySlug[alunoFoco.id] || undefined,
           }
         : null,
      historico: historicoFiltrado,
