c1d42539d69190155979ade74172c70b4c78ddb3
Pedro <pedrojr.ferreiraa@gmail.com>
2026-06-08 10:24:02 -0300

    Update PEIFormModal.tsx

--- src/components/inclusao/PEIFormModal.tsx
+++ src/components/inclusao/PEIFormModal.tsx
@@ -149,6 +149,7 @@ function blankPEI(): PEIData {
     responsavelContato: "",
     dataInicioPEI: "",
     profissionaisEnvolvidos: [],
+    profissionaisNomes: "",
     potencialidades: "",
     interessesMotivacoes: "",
     estiloAprendizagem: [],
